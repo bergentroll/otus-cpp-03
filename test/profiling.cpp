b64c8080983a0f361795c881dca49a2e5f1f3b0c
Anton Karmanov <bergertroll@insiberia.net>
2020-05-05 16:12:28 +0500

    Update profiling

--- test/profiling.cpp
+++ test/profiling.cpp
@@ -5,45 +5,46 @@
 
 constexpr size_t SIZE { 10000 };
 
-template <template<typename, typename> typename T, typename I, typename Alloc>
-void run(T<I, Alloc> &t) {
+template <typename T>
+void run_container() {
+  T t { };
   for (size_t i { }; i < SIZE; i++) {
     t.push_back(i);
   }
 }
 
-template <typename T, typename K, typename Less, class Alloc>
-void run_m(std::map<T, K, Less, Alloc> &m) {
+template <typename T>
+void run_map() {
+  T t { };
   for (size_t i { }; i < SIZE; i++) {
-    m[i] = i * i;
+    t[i] = i * i;
   }
 }
 
 static void run_container(benchmark::State& state) {
   for (auto _ : state) {
-    otus::Container<int> c;
-    run(c);
+    run_container<otus::Container<int>>();
   }
 }
 
 static void run_container_allocator(benchmark::State& state) {
   for (auto _ : state) {
-    otus::Container<int, otus::Allocator<int, SIZE>> c;
-    run(c);
+      run_container<otus::Container<int, otus::Allocator<int, SIZE>>>();
   }
 }
 
 static void run_map(benchmark::State& state) {
   for (auto _ : state) {
     std::map<int, int> m;
-    run_m(m);
+    run_map<std::map<int, int>>();
   }
 }
 
 static void run_map_allocator(benchmark::State& state) {
   for (auto _ : state) {
-    std::map<int, int, std::less<int>, otus::Allocator<std::pair<int, int>, SIZE>> m;
-    run_m(m);
+    run_map<
+      std::map<int, int, std::less<int>,
+      otus::Allocator<std::pair<int, int>, SIZE>>>();
   }
 }
 
